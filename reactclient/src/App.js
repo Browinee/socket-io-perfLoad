3a09636a1860cd882a086a5cc9fc912b6583b5cc
justin.lin <justin.lin@yxunistar.com>
2021-07-16 22:34:15 +0800

    feat: add react client

---
+++ reactclient/src/App.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect} from 'react';
+import './App.css';
+import socket from './utilities/socketConnection';
+import Widget from './component/widget';
+
+
+function App() {
+    const [performanceData, setPerf] = useState({});
+    useEffect(() => {
+        socket.on('data', (data) => {
+            setPerf((prevPerf) => {
+                return {
+                    ...prevPerf,
+                    [data.macA]: data
+                }
+            });
+        })
+    }, []);
+    console.log("perf", performanceData)
+    return (
+        <div className="App">
+            {
+                Object.entries(performanceData).map(([key, value], index) => {
+                    return <Widget key={key} data={value}/>
+                })
+            }
+        </div>
+    );
+}
+
+
+export default App;
